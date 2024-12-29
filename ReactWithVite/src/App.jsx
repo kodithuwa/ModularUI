045583afd432cca4e47db2b7e0149e0e17fc42fd
Your Name <you@example.com>
2024-12-29 19:41:25 +0200

    React With Vite

---
+++ ReactWithVite/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>Hello, React with Vite!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App;
